3a3ee86c33805da03959970ecb831c5999e0a0d0
Vikitohyah <victoriaedorh@gmail.com>
2024-03-01 15:48:01 +0000

    Friends Connect

---
+++ data.js
@@ -0,0 +1,34 @@
+export default [
+  null,
+  {
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/bc/6d/d8/bc6dd84ab48f40e09f5b4f0409c5471c.jpg',
+    name: 'Mawusi Amele',
+    age: 26,
+    description: 'Christian Model'
+  },
+  {
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/5c/a0/f1/5ca0f13dd50ed9f6d630d8a7d7da0f3e.jpg',
+    name: 'Abidemi Coker',
+    age: 35,
+    description: 'People are the beauty of life'
+  },
+  {
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/c4/67/49/c467498720c056c241dc1226f7290597.jpg',
+    name: 'Desiree',
+    age: 24,
+    description: 'Je suis tres bien et toi!'
+  },
+  {
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/76/90/7d/76907d454d4cb62f07b4fdf92ae05371.jpg',
+    name: 'Alexis',
+    age: 32,
+    description: 'The greatest showgirl'
+  },
+  {
+    image: 'https://s-media-cache-ak0.pinimg.com/564x/62/86/33/6286333a4f160a24b11e6a7ee8ea0b38.jpg',
+    name: 'Caroline',
+    age: 22,
+    description: "I love cats and a big fan of Dolly Partons"
+  },
+  null
+];
