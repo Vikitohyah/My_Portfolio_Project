e3ec5e4568a34880db11b54d523072cdcfe42a24
Vikitohyah <victoriaedorh@gmail.com>
2024-03-01 19:33:38 +0000

    images for the app in data.js

--- data.js
+++ data.js
@@ -1,31 +1,31 @@
 export default [
   null,
   {
-    image: 'https://www.canva.com/design/DAF-SF6ugSs/I9NMIpWGxlaTcJmfMkGTnw/view?utm_content=DAF-SF6ugSs&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+    image: 'https://drive.google.com/file/d/1B7ZVkFAvZ9JqFzDWCIBt9Xt4VzTY4NGt/view?usp=drivesdk',
     name: 'Racheal Smith',
     age: 26,
     description: 'Clearly your Christian Model'
   },
   {
-    image: 'https://www.canva.com/design/DAF-SD1ENXM/NR6wEYFxzLd8v1yv7duf3A/view?utm_content=DAF-SD1ENXM&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+    image: 'https://drive.google.com/file/d/1B9bEdQXKerGWxXE3WkIVPow7iWsSCwSK/view?usp=drivesdk',
     name: 'Kemisola Coker',
     age: 25,
     description: 'The greatest Showgirl'
   },
   {
-    image: 'https://www.canva.com/design/DAF-SdXvTKU/eRSCbkuxLZ2-ko1H_Me4ww/view?utm_content=DAF-SdXvTKU&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+    image: 'https://drive.google.com/file/d/1B9bEdQXKerGWxXE3WkIVPow7iWsSCwSK/view?usp=drivesdk',
     name: 'Harrison Smart',
     age: 27,
     description: 'Je suis tres bien et toi!'
   },
   {
-    image: 'https://www.canva.com/design/DAF-SsQL6Sk/JhZf2F1H7LsxGdsOixqVDA/view?utm_content=DAF-SsQL6Sk&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+    image: 'https://drive.google.com/file/d/1BJDf_T0SUjFcqTZa_NKZEgV8P_f2YwML/view?usp=drivesdk',
     name: 'Victoria Ajay',
     age: 19,
     description: 'People are the beauty of life'
   },
   {
-    image: 'https://www.canva.com/design/DAF-Sj1Dk_8/v3xoJB3g5uUH0iaV_8aJoQ/view?utm_content=DAF-Sj1Dk_8&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+    image: 'https://drive.google.com/file/d/1BIgWP4dGVs2NqAeGbMRGy5xyzjGXM0IH/view?usp=drivesdk',
     name: 'David York',
     age: 21,
     description: "Love cats and a big fan of Burna Boy"
